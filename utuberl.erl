16bc5f4ecd9e729c59731e86bf2a8dd07ce8419b
Benjamin Kampmann <ben.kampmann@googlemail.com>
2009-07-23 19:16:54 +0200

    documenation and readibility

--- utuberl.erl
+++ utuberl.erl
@@ -18,11 +18,21 @@
 
 parse_youtube_video(Body) ->
   Tree = mochiweb_html:parse(Body),
+  %% alright. let's find stuff. first match is enough
+  %% FIXME: actually if the first one was found the execution could stop as we
+  %%        are happy already but AFAIK that ain't supported by mochiweb_xpath
+  %%        yet
+  [Title| _] = mochiweb_xpath:execute(".//div[@id=\"watch-vid-title\"]/h1/text()", Tree),
   [Url|_] = mochiweb_xpath:execute(".//input[@id=\"watch-url-field\"]/@value", Tree),
   [Embed|_] = mochiweb_xpath:execute(".//input[@id=\"embed_code\"]/@value", Tree),
-  [Title| _] = mochiweb_xpath:execute(".//div[@id=\"watch-vid-title\"]/h1/*", Tree),
-  Tags = mochiweb_xpath:execute(".//div[@id=\"watch-video-tags\"]/a/*", Tree),
+  %% there we actually want the full list
+  Tags = mochiweb_xpath:execute(".//div[@id=\"watch-video-tags\"]/a/text()", Tree),
+  %% no we do something intresting: we know the code inside of "Embed" is html
+  %% again so we parse it and use xpath once more to find the URL
   [Video_url| _] = mochiweb_xpath:execute(".//param[@name=\"movie\"]/@value", mochiweb_html:parse(Embed)),
+  %% and then find the totally correct video id inside of that URL
+  %% FIXME: is this really a value of interest or is this useless and the code
+  %%        with it?
   V_url = binary_to_list(Video_url),
   Video_id = string:sub_string(V_url, string:rstr(V_url, "/") + 1),
   {ok, #utube_video{title=Title,url=Url,tags=Tags,
